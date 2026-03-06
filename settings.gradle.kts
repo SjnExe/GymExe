3d0ab04dfdd32a1d8245ae3d017685b3f5546bf4
SjnExe <sjnexecontact@gmail.com>
2026-03-06 23:48:14 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     includeBuild("build-logic")
     plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     }
     repositories {
         google()
