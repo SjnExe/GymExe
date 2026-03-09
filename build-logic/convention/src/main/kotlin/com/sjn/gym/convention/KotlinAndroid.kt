f2784cf2ca9ed28e9f412b08d3465fcee694a792
SjnExe <sjnexecontact@gmail.com>
2026-03-09 23:08:38 +0530

    Update KotlinAndroid.kt

--- build-logic/convention/src/main/kotlin/com/sjn/gym/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/sjn/gym/convention/KotlinAndroid.kt
@@ -1,7 +1,6 @@
 package com.sjn.gym.convention
 
 import com.android.build.api.dsl.ApplicationExtension
-import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
@@ -13,7 +12,7 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
+internal fun Project.configureKotlinAndroid(commonExtension: Any) {
     if (commonExtension is ApplicationExtension) {
         commonExtension.compileOptions {
             sourceCompatibility = JavaVersion.VERSION_25
