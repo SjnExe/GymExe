d877bcb562742ff23419cde48beea9b1a89cc6ea
SjnExe <sjnexecontact@gmail.com>
2026-03-09 20:03:21 +0530

    Update AndroidApplicationConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/com/sjn/gym/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/sjn/gym/convention/AndroidApplicationConventionPlugin.kt
@@ -12,6 +12,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 apply("com.android.application")
                 apply("gymexe.spotless")
                 apply("gymexe.kover")
+                apply("com.autonomousapps.dependency-analysis")
             }
 
             extensions.configure<ApplicationExtension> {
