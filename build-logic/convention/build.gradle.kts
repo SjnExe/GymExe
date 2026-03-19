c6ae7d2f96dfd7a8694e94f827331c6b74d33789
SjnExe <muhammedshajahan100@gmail.com>
2026-03-19 13:45:46 +0530

    commit

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation(libs.kover.gradlePlugin)
     implementation(libs.roborazzi.gradlePlugin)
     implementation(libs.spotless.gradlePlugin)
-    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:2.0.2")
+    implementation(libs.dependencyAnalysis.gradlePlugin)
 }
 
 gradlePlugin {
