de5230d265c56e26df58870c89d361b787a20904
SjnExe <muhammedshajahan100@gmail.com>
2026-03-23 00:34:21 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -14,15 +13,19 @@ plugins {
 }
 
 dependencyAnalysis {
+    val warnOnly = project.hasProperty("warnDependencies")
+    val printToConsole = project.hasProperty("printDependencies")
+
     issues {
         all {
-            onUnusedDependencies {
-                severity("warn")
-            }
             onAny {
-                severity("warn")
+                severity(if (warnOnly) "warn" else "fail")
             }
         }
+    }
+    
+    reporting {
+        printBuildHealth(printToConsole)
     }
 }
 
